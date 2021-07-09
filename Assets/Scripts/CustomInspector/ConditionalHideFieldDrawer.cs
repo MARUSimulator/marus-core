94caf6b1be08580cc80af2c1fdf90332a8653239
mandicLuka <luka.mandic311@gmail.com>
2021-07-09 14:03:16 +0200

    Implement simulator control service
    Fix bugs in parameter server
    Add custom inspector for conditionally drawing fields
    Add possibility for running simulation faster/slower than real-time

---
+++ Assets/Scripts/CustomInspector/ConditionalHideFieldDrawer.cs
@@ -0,0 +1,111 @@
+using UnityEngine;
+using UnityEditor;
+
+
+[CustomPropertyDrawer(typeof(HideInRuntimeInspectorAttribute))]
+[CustomPropertyDrawer(typeof(ConditionalHideInInspectorAttribute))]
+public class ConditionalHidePropertyDrawer : PropertyDrawer
+{
+    public override void OnGUI( Rect position, SerializedProperty property, GUIContent label )
+    {
+        var enabled = GetConditionalHideAttributeResult(attribute, property);
+
+        var wasEnabled = GUI.enabled;
+        GUI.enabled = enabled;
+        if (enabled)
+        {
+            EditorGUI.PropertyField(position, property, label, true);
+        }
+
+        GUI.enabled = wasEnabled;
+    }
+
+    public override float GetPropertyHeight( SerializedProperty property, GUIContent label )
+    {
+        var enabled = GetConditionalHideAttributeResult(attribute, property);
+
+        if (enabled)
+        {
+            return EditorGUI.GetPropertyHeight(property, label);
+        }
+        else
+        {
+            //The property is not being drawn
+            //We want to undo the spacing added before and after the property
+            return -EditorGUIUtility.standardVerticalSpacing;
+        }
+    }
+
+    private bool GetConditionalHideAttributeResult(PropertyAttribute propertyAttr, SerializedProperty property )
+    {
+        var enabled = true;
+
+        if (propertyAttr is HideInRuntimeInspectorAttribute hideInRuntime)
+        {
+            return !EditorApplication.isPlaying;
+        }
+
+        if (propertyAttr is ConditionalHideInInspectorAttribute condHAtt)
+        {
+            var propertyPath =
+                property.propertyPath; //returns the property path of the property we want to apply the attribute to
+            string conditionPath;
+
+            if (!string.IsNullOrEmpty(condHAtt.ConditionalSourceField))
+            {
+                //Get the full relative property path of the sourcefield so we can have nested hiding
+                conditionPath =
+                    propertyPath.Replace(property.name,
+                        condHAtt.ConditionalSourceField); //changes the path to the conditionalsource property path
+                var sourcePropertyValue = property.serializedObject.FindProperty(conditionPath);
+
+                if (sourcePropertyValue != null)
+                {
+                    enabled = CheckPropertyType(sourcePropertyValue);
+                }
+                else
+                {
+                    //Debug.LogWarning("Attempting to use a ConditionalHideInInspectorAttribute but no matching SourcePropertyValue found in object: " + condHAtt.ConditionalSourceField);
+                }
+            }
+
+            if (!string.IsNullOrEmpty(condHAtt.ConditionalSourceField2))
+            {
+                conditionPath =
+                    propertyPath.Replace(property.name,
+                        condHAtt.ConditionalSourceField2); //changes the path to the conditionalsource property path
+                var sourcePropertyValue2 = property.serializedObject.FindProperty(conditionPath);
+
+                if (sourcePropertyValue2 != null)
+                {
+                    enabled = enabled && CheckPropertyType(sourcePropertyValue2);
+                }
+                else
+                {
+                    //Debug.LogWarning("Attempting to use a ConditionalHideInInspectorAttribute but no matching SourcePropertyValue found in object: " + condHAtt.ConditionalSourceField);
+                }
+            }
+
+            if (condHAtt.Inverse) enabled = !enabled;
+
+            return enabled;
+
+        }
+        return true;
+    }
+
+    private bool CheckPropertyType( SerializedProperty sourcePropertyValue )
+    {
+        switch ( sourcePropertyValue.propertyType )
+        {
+            case SerializedPropertyType.Boolean:
+                return sourcePropertyValue.boolValue;
+            case SerializedPropertyType.ObjectReference:
+                return sourcePropertyValue.objectReferenceValue != null;
+            default:
+                Debug.LogError("Data type of the property used for conditional hiding [" +
+                                sourcePropertyValue.propertyType + "] is currently not supported");
+                return true;
+        }
+    }
+}
