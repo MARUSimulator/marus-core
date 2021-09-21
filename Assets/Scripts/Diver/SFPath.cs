2b7a35c12fdb671c7542055ea3f263df00d1b3a8
Gyula Nagy <dula.nad@fer.hr>
2021-09-21 13:43:03 +0200

    Added skeleton for SF path display. Adjusted D2 prefab.

---
+++ Assets/Scripts/Diver/SFPath.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SFPath : MonoBehaviour
+{
+    public LineRenderer lineRenderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+        //lineRenderer.SetColors(c1, c2);
+        lineRenderer.SetWidth(0.2F, 0.2F);
+        int i = 0;
+        var theta_scale = 0.1f;
+        lineRenderer.SetVertexCount(Convert.ToInt32(Math.Ceiling(2*Math.PI/theta_scale + 1)));
+        var r = 3.0f;
+        for(float theta = 0; theta <= 2 * Math.PI + theta_scale; theta += theta_scale) {
+            var x = (float)(r*Math.Cos(theta));
+            var y = (float)(r*Math.Sin(theta));
+
+            Vector3 pos = new Vector3(x, 0 , y);
+            lineRenderer.SetPosition(i, pos);
+            i+=1;
+        }
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
