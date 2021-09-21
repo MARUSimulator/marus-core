6b7ebebd572cca06baa970535d8719fadddee032
natkokrasevac <natko.krasevac@fer.hr>
2021-09-21 10:25:55 +0200

    Render topdown camera only when statistics canvas is active

--- Assets/Scripts/StatisticsUI/StatisticsUIController.cs
+++ Assets/Scripts/StatisticsUI/StatisticsUIController.cs
@@ -9,12 +9,15 @@ namespace Labust.StatisticsUI
     public class StatisticsUIController : MonoBehaviour
     {
         private Canvas _canvas;
+        private GameObject _cameraObject;
         private PathRecordingsVisualization _controller;
 
         void Start()
         {
+            _cameraObject = transform.Find("Panel/TopDownCamera").gameObject;
             _canvas = GetComponent<Canvas>();
             _canvas.enabled = false;
+            _cameraObject.SetActive(false);
             _controller = GetComponentInChildren<PathRecordingsVisualization>();
         }
 
@@ -24,10 +27,12 @@ namespace Labust.StatisticsUI
             {
                 if (_canvas.enabled)
                 {
+                    _cameraObject.SetActive(false);
                     _canvas.enabled = false;
                 }
                 else
                 {
+                    _cameraObject.SetActive(true);
                     _canvas.enabled = true;
                     _controller.RefreshPaths();
                 }
