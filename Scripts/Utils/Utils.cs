39cdfdf39d0936718c843335dff77b26f15a932d
natkokrasevac <natko.krasevac@fer.hr>
2022-04-13 11:43:25 +0200

    Add documentation and fix some bugs

--- Scripts/Utils/Utils.cs
+++ Scripts/Utils/Utils.cs
@@ -1,4 +1,4 @@
-// Copyright 2022 Laboratory for Underwater Systems and Technologies (LABUST)
+// Copyright 2021 Laboratory for Underwater Systems and Technologies (LABUST)
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
