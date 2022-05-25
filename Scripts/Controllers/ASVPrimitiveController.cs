76145146a6cdbd8f0347e05290fcf9233ec43b25
Juraj <obradovic.juraj@gmail.com>
2022-05-25 10:36:41 +0200

    Switch A and D key

--- Scripts/Controllers/ASVPrimitiveController.cs
+++ Scripts/Controllers/ASVPrimitiveController.cs
@@ -68,8 +68,8 @@ public class ASVPrimitiveController : MonoBehaviour
 
         { // rotation
             float rotate = 0f;
-            rotate += (key == KeyCode.A ? 1 : 0);
-            rotate -= (key == KeyCode.D ? 1 : 0);
+            rotate += (key == KeyCode.D ? 1 : 0);
+            rotate -= (key == KeyCode.A ? 1 : 0);
             Vector3 ea = _targetTransform.eulerAngles;
             ea.y += 0.1f * rotSpeed * rotate * dt;
             _targetTransform.eulerAngles = ea;
